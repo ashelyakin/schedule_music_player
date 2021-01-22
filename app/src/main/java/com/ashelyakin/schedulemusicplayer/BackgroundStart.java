7ded4721bf10dda2958dfc780454a610da2465f7
ashelyakin <andrew.shelyakin980@gmail.com>
2021-01-22 15:06:02 +0300

    WIP

---
+++ app/src/main/java/com/ashelyakin/schedulemusicplayer/BackgroundStart.java
@@ -0,0 +1,46 @@
+package com.ashelyakin.schedulemusicplayer;
+
+import android.app.AppOpsManager;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
+
+import java.lang.reflect.Method;
+
+public class BackgroundStart {
+
+    public static boolean canBackgroundStart(Context context) {
+        AppOpsManager ops = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
+        try {
+            int op = 10021; // >= 23
+            // ops.checkOpNoThrow(op, uid, packageName)
+            Method method = ops.getClass().getMethod("checkOpNoThrow", new Class[]
+                    {int.class, int.class, String.class}
+            );
+            Integer result = (Integer) method.invoke(ops, op, android.os.Process.myUid(), context.getPackageName());
+            return result == AppOpsManager.MODE_ALLOWED;
+        } catch (Exception e) {
+            Log.e("BackgroundStart", "not support", e);
+        }
+        return false;
+    }
+
+    public static boolean isAccessGranted(Context context) {
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            ApplicationInfo applicationInfo = packageManager.getApplicationInfo(context.getPackageName(), 0);
+            AppOpsManager appOpsManager = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
+            int mode = 0;
+            if (android.os.Build.VERSION.SDK_INT > android.os.Build.VERSION_CODES.KITKAT) {
+                mode = appOpsManager.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS,
+                        applicationInfo.uid, applicationInfo.packageName);
+            }
+            return (mode == AppOpsManager.MODE_ALLOWED);
+
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+}
--- app/src/main/java/com/ashelyakin/schedulemusicplayer/activity/PlaybackActivity.kt
+++ app/src/main/java/com/ashelyakin/schedulemusicplayer/activity/PlaybackActivity.kt
@@ -2,9 +2,11 @@ package com.ashelyakin.schedulemusicplayer.activity
 
 import android.app.ActivityManager
 import android.app.ActivityManager.RecentTaskInfo
+import android.app.AlarmManager
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -15,12 +17,13 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
+import com.ashelyakin.schedulemusicplayer.BackgroundStart
 import com.ashelyakin.schedulemusicplayer.ForegroundCallbacks
 import com.ashelyakin.schedulemusicplayer.PlayerApplication
 import com.ashelyakin.schedulemusicplayer.R
-import com.ashelyakin.schedulemusicplayer.Receiver
 import com.ashelyakin.schedulemusicplayer.player.ExoPlayerListener
 import com.ashelyakin.schedulemusicplayer.player.PlayerViewModel
 import com.ashelyakin.schedulemusicplayer.profile.Schedule
@@ -57,30 +60,47 @@ class PlaybackActivity: AppCompatActivity() {
         PlayerApplication.setForegroundedActivity(object: ForegroundCallbacks{
             override fun start() {
                 runOnUiThread {
-                    //launchApp("com.ashelyakin.schedulemusicplayer")
-                    //activityManager.moveTaskToFront(taskId, 0)
-                    /*val startForegroundedActivityIntent = packageManager.getLaunchIntentForPackage(packageName)
-                    val pStartForegroundedActivityIntent = PendingIntent.getActivity(context, 0, startForegroundedActivityIntent, 0)
-                    val am = getSystemService(ALARM_SERVICE) as AlarmManager
-                    am.set(AlarmManager.RTC, System.currentTimeMillis(), pStartForegroundedActivityIntent)*/
-                    val startForegroundedActivityIntent = Intent(context, MainActivity::class.java)
-                    startForegroundedActivityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);//optional
-                    startForegroundedActivityIntent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);//will cause it to come to foreground
-                    startForegroundedActivityIntent.putExtra("ID_TimeLeft","test");
-                    startActivity(startForegroundedActivityIntent);
+                    /*val fullScreenIntent = Intent(context, PlaybackActivity::class.java)
+                    val fullScreenPendingIntent = PendingIntent.getActivity(context, 0,
+                            fullScreenIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+                    val notificationBuilder = NotificationCompat.Builder(context)
+                            .setSmallIcon(R.drawable.ic_launcher)
+                            .setContentTitle("Incoming call")
+                            .setContentText("(919) 555-1234") //The following are the key 3 lines
+                            .setPriority(NotificationCompat.PRIORITY_HIGH)
+                            .setCategory(NotificationCompat.CATEGORY_CALL)
+                            .setFullScreenIntent(fullScreenPendingIntent, true)
+
+                    val notifyManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                    notifyManager.notify(7777, notificationBuilder.build())*/
+
+                    val startForegroundedActivityIntent = Intent(context, PlaybackActivity::class.java)
+                    val showOperation = PendingIntent.getActivity(context, 0, startForegroundedActivityIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+                    val alarmClockInfo = AlarmManager.AlarmClockInfo(System.currentTimeMillis() + 1000, showOperation)
+                    val am = context!!.getSystemService(AppCompatActivity.ALARM_SERVICE) as AlarmManager
+                    am.setAlarmClock(alarmClockInfo, showOperation)
 
                 }
             }
         })
         //initPlayer()
-        if (if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    !Settings.canDrawOverlays(this)
-
-                } else {
-                    TODO("VERSION.SDK_INT < M")
-                }) {
-            val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName"))
-            startActivityForResult(intent, 0)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                //Request permission if not authorized
+                val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
+                intent.data = Uri.parse("package:$packageName")
+                startActivityForResult(intent, 0)
+            }
+        }
+        if (!BackgroundStart.isAccessGranted(this)) {
+            Log.e(TAG, "!isAccessGranted")
+            val intent = Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS)
+            startActivity(intent)
+        }
+        if (!BackgroundStart.canBackgroundStart(context)) {
+            Log.e(TAG, "!canBackgroundStart")
+            startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
         }
         Log.d(TAG, "initPlayer was completed")
     }
